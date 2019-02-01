137d50dbf3150f72d27c1902213d8f44ec9f5aa0
Marcelo Glacial <glacial@gmail.com>
2019-02-01 02:09:08 -0500

    Add smooth scrolling

--- front-end/src/assets/js/main.js
+++ front-end/src/assets/js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    $("#services ul li a").click(function (event) {
-        event.preventDefault();
-    });
 
     // Map behaviour
     $('#map').addClass('off');
@@ -12,4 +9,18 @@ $(document).ready(function () {
         .mouseleave(function () {
             $(this).addClass('off')
         });
+
+    // Smooth Scroll 
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 600);
+    });
+    $("#services ul li a").click(function (event) {
+        event.preventDefault();
+    });
+
 });
+
